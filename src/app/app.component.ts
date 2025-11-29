2a754d5517c234f132fb53e4e9f1aec5c6b33e68
Tempi <159011940+MarcinLewczuk@users.noreply.github.com>
2025-11-29 13:12:07 +0000

    chore: separated header/footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLinkWithHref],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
   title = 'Commerse';
-  currentYear: number = new Date().getFullYear();
 }
